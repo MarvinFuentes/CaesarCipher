02048ce79604742d0342b10426817e0771b397cc
marvi <mfuentes1@ggc.edu>
2023-04-25 19:52:48 -0400

    LAST

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class CaesarCipher {
+/**
+ * @Class: CeaserCipher
+ * @Aurthor: Marvin Fuentes
+ * @Course: ITEC 2140 Section 4
+ * @Written: April 25, 2023
+ * This class does not contain a main method since all I need this class to do is
+ * identify the integer the user will want to use in the main method and determine how
+ * much to shift the alphabet. The user will determine if they want to use to encrypt
+ * or decrypt method to further more encrypt a message or decrypt it. The for loop begins
+ * by declaring a String variable as a placeholder and the for loop will use the index
+ * from the input message to shift it in the alphabet then return the result.
+ */
+class CaesarCipher {
+    private int integer;
+    //This is the constructor
+    public CaesarCipher(int integer) {
+        this.integer = integer;
+    }
+    //If the user prompts the program to encrypt then this method will be invoked.
+    public String encrypt(String message) {
+        String encrypted = "";
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+            if (Character.isLetter(c)) {
+                char shift = (char) (c + integer);
+                if (Character.isUpperCase(c) && shift > 'Z') {
+                    shift = (char) (shift - 26);
+                } else if (Character.isLowerCase(c) && shift > 'z') {
+                    shift = (char) (shift - 26);
+                }
+                encrypted = encrypted + shift;
+            } else {
+                encrypted = encrypted + c;
+            }
+        }
+        return encrypted;
+    }
+    //If the user prompts the program to decrypt then this method will be invoked.
+    public String decrypt(String message) {
+        String decrypted = "";
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+            if (Character.isLetter(c)) {
+                char shift = (char) (c - integer);
+                if (Character.isUpperCase(c) && shift < 'A') {
+                    shift = (char) (shift + 26);
+                } else if (Character.isLowerCase(c) && shift < 'a') {
+                    shift = (char) (shift + 26);
+                }
+                decrypted = decrypted + shift;
+            } else {
+                decrypted = decrypted + c;
+            }
+        }
+        return decrypted;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Main {
+/**
+ * @Class: Main
+ * @Aurthor: Marvin Fuentes
+ * @Course: ITEC 2140 Section 4
+ * @Written: April 25, 2023
+ * This class is called the main class since it contains the main method that will
+ * create an instance to call the CaesarCipher Class and the user will also determine
+ * which method will be invoked depending on their result. The program will also ask
+ * the user to enter an integer which is technically the key to the cipher and will be
+ * also used in the CaesarCipher class. Finally, the program will prompt the user with
+ * the encrypted message or the decrypted message.
+ */
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        //Prompt the user to enter their encrypted message or message they want to encrypt.
+        System.out.print("Enter a message to be encrypted or decrypted: ");
+        String message = scanner.nextLine();
+        //The program then displays the message for user to enter the key integer value.
+        System.out.print("Enter an integer key value for encryption or decryption: ");
+        int integer = scanner.nextInt();
+        //Right here I create an instance for the CaesarCipher Class.
+        CaesarCipher cipher = new CaesarCipher(integer);
+        //The program will now ask the user if they want to encrypt or decrypt the message.
+        System.out.print("Enter 'E' to encrypt or 'D' to decrypt: ");
+        String encryptOrDecrypt = scanner.next();
+        //Based of the integer the user inputs the class will call the decrypt or encrypt method.
+        String answer = "";
+        if (encryptOrDecrypt.equalsIgnoreCase("e")) {
+            answer = cipher.encrypt(message);
+        } else if (encryptOrDecrypt.equalsIgnoreCase("d")) {
+            answer = cipher.decrypt(message);
+        }
+        else {
+            //This is just in case the user enters a letter or number that isn't e or d.
+            System.out.println("Invalid input. Please enter 'e' or 'd'.");
+            return;
+        }
+        //Now the program will display the encrypted or decrypted message.
+        System.out.println("Decrypted or Encrypted message: " + answer);
+    }
 }
+
